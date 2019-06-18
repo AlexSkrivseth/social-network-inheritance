fb8cfcad9617b0c91dbf7821f0175467e04df167
ALEX SKRIVSETH <alexskrivseth@ALEXs-MBP.netgear.com>
2019-06-17 22:32:09 -0700

    alex skrivseth 06/17/2019

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,28 @@
 
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.posts = []
+        self.following = []
 
     def add_post(self, post):
-        pass
-
+        # call post.set_user(self) sometime in this method
+        self.posts.append(post)
+        post.set_user(self)
+    
     def get_timeline(self):
-        pass
+        time_line_posts = []
+        for user in self.following:
+            for post in user.posts:
+                time_line_posts.append(post)
+        # how to sort time_line_posts by timestamp? 
+        return time_line_posts
+        
+
+
 
     def follow(self, other):
-        pass
+        self.following.append(other)
+    
